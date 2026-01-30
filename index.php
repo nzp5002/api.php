97f41fb6e5cac98d5f8ed1ebde1edb207586eacc
nzp5002 <antonio.danilo3652024@gmail.com>
2026-01-30 03:53:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,11 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, ngrok-skip-browser-warning");
-
+// RESPONDE O PREFLIGHT
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
 require_once 'Database.php';
 require_once 'AcademiaService.php';
 
