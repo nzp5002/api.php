ca20c89f3b151fb834065c2eabd7b36ad6d8852f
nzp5002 <antonio.danilo3652024@gmail.com>
2026-01-30 04:58:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+require_once 'Database.php';
+require_once 'AcademiaService.php'; //
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
